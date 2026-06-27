bc3996142a458c4c932aaebd21dc360e9dc59943
Ayman <aymanq12@gmail.com>
2026-06-28 01:19:55 +0300

    wedding qr system

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// *** امليء هذه القيم من إعدادات مشروعك على Firebase ***
+const firebaseConfig = {
+  apiKey:            "AIzaSyAmqV7g32Ui_q7sFmj7H5mOMrewjtn4pjk",
+  authDomain:        "wedding-qr-dd5aa.firebaseapp.com",
+  databaseURL:       "https://wedding-qr-dd5aa-default-rtdb.firebaseio.com",
+  projectId:         "wedding-qr-dd5aa",
+  storageBucket:     "wedding-qr-dd5aa.firebasestorage.app",
+  messagingSenderId: "994055652336",
+  appId:             "1:994055652336:web:6bbb553799ab7bd5f9d335"
+};
